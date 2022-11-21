093120f002b2deec32792b8b553f8fae78223c4a
Ryan Liu <azhai@126.com>
2022-11-22 01:09:45 +0800

    重写logging

--- go.mod
+++ go.mod
@@ -6,21 +6,21 @@ require (
 	github.com/arriqaaq/flashdb v0.1.6
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gobwas/glob v0.2.3
-	github.com/gomodule/redigo v1.8.8
+	github.com/gomodule/redigo v1.8.9
 	github.com/grsmv/inflect v0.0.0-20140723132642-a28d3de3b3ad
-	github.com/hashicorp/hcl/v2 v2.12.0
+	github.com/hashicorp/hcl/v2 v2.15.0
 	github.com/json-iterator/go v1.1.12
 	github.com/k0kubun/pp v3.0.1+incompatible
-	github.com/klauspost/cpuid/v2 v2.0.13
-	github.com/lib/pq v1.10.6
+	github.com/klauspost/cpuid/v2 v2.2.1
+	github.com/lib/pq v1.10.7
 	github.com/manifoldco/promptui v0.9.0
-	github.com/mattn/go-sqlite3 v1.14.13
+	github.com/mattn/go-sqlite3 v1.14.16
 	github.com/mitchellh/copystructure v1.2.0
 	github.com/pkg/errors v0.9.1
-	github.com/stretchr/testify v1.7.2
-	go.uber.org/zap v1.21.0
-	golang.org/x/tools v0.1.11
-	xorm.io/xorm v1.3.1
+	github.com/stretchr/testify v1.8.1
+	go.uber.org/zap v1.23.0
+	golang.org/x/tools v0.3.0
+	xorm.io/xorm v1.3.2
 )
 
 require (
@@ -31,26 +31,27 @@ require (
 	github.com/arriqaaq/hash v0.1.2 // indirect
 	github.com/arriqaaq/set v0.1.2 // indirect
 	github.com/arriqaaq/zset v0.1.2 // indirect
-	github.com/chzyer/readline v1.5.0 // indirect
+	github.com/chzyer/readline v1.5.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/syndtr/goleveldb v1.0.0 // indirect
-	github.com/zclconf/go-cty v1.10.0 // indirect
-	go.uber.org/atomic v1.9.0 // indirect
+	github.com/zclconf/go-cty v1.12.1 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
-	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
-	golang.org/x/sys v0.0.0-20220610221304-9f5ed59c137d // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/mod v0.7.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	xorm.io/builder v0.3.11 // indirect
+	xorm.io/builder v0.3.12 // indirect
 )
---
+++ utils/logging/build.go
@@ -0,0 +1,157 @@
+package logging
+
+import (
+	"fmt"
+	"io"
+	"net/url"
+	"path/filepath"
+	"strings"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// Output 输出配置
+type Output struct {
+	Start, Stop string
+	OutPaths    []string
+}
+
+type LogConfig struct {
+	zap.Config
+	MinLevel   string
+	LevelCase  string
+	TimeFormat string
+	Outputs    []Output
+}
+
+func NewLogger(cfg LogConfig, dir string) *zap.SugaredLogger {
+	if zl, err := cfg.BuildLogger(dir); err == nil {
+		return zl.Sugar()
+	}
+	return nil
+}
+
+func SingleFileConfig(level, file string) LogConfig {
+	cfg := NewDefaultConfig()
+	cfg.MinLevel = level
+	cfg.Outputs = []Output{
+		{Start: level, Stop: "", OutPaths: []string{file}},
+	}
+	return cfg
+}
+
+func NewDefaultConfig() LogConfig {
+	return LogConfig{
+		Config: zap.Config{
+			Encoding:         "console",
+			OutputPaths:      []string{},
+			ErrorOutputPaths: []string{"stderr"}, // zap内部错误输出
+		},
+		MinLevel:   "debug",
+		LevelCase:  "capital",
+		TimeFormat: "2006-01-02 15:04:05",
+		Outputs: []Output{
+			{Start: "debug", Stop: "info", OutPaths: []string{"access.log"}},
+			{Start: "warn", Stop: "fatal", OutPaths: []string{"error.log"}},
+		},
+	}
+}
+
+func (c LogConfig) IsNop() bool {
+	return len(c.Outputs) == 0 && len(c.OutputPaths) == 0
+}
+
+func (c LogConfig) BuildLogger(dir string, opts ...zap.Option) (*zap.Logger, error) {
+	if c.IsNop() {
+		return zap.NewNop(), nil
+	}
+	if c.GetLevel().Enabled(zapcore.InfoLevel) {
+		c.Development = true
+		c.Sampling = nil
+	}
+	dir = strings.TrimSpace(dir)
+	if cores := c.GetCores(dir); len(cores) > 1 {
+		opts = append(opts, ReplaceCores(cores))
+	}
+	return c.Config.Build(opts...)
+}
+
+func (c LogConfig) GetLevel() zap.AtomicLevel {
+	var level zapcore.Level
+	c.MinLevel, level = GetZapLevel(c.MinLevel)
+	c.Level = zap.NewAtomicLevelAt(level)
+	return c.Level
+}
+
+func (c LogConfig) GetCores(dir string) []zapcore.Core {
+	var (
+		cores []zapcore.Core
+		ws    zapcore.WriteSyncer
+		err   error
+	)
+	enc := c.GetEncoder()
+	for _, out := range c.Outputs {
+		enab := GetLevelEnabler(out.Start, out.Stop, c.MinLevel)
+		if enab == nil || len(out.OutPaths) == 0 {
+			continue
+		}
+		c.OutputPaths = GetLogPath(dir, out.OutPaths)
+		if len(c.OutputPaths) == 0 || c.OutputPaths[0] == "/dev/null" {
+			ws = zapcore.AddSync(io.Discard)
+		} else if ws, _, err = zap.Open(c.OutputPaths...); err != nil {
+			continue
+		}
+		cores = append(cores, zapcore.NewCore(enc, ws, enab))
+	}
+	return cores
+}
+
+func (c LogConfig) GetEncoder() zapcore.Encoder {
+	config := NewEncoderConfig(c.TimeFormat, c.LevelCase)
+	if strings.ToLower(c.Encoding) == "json" {
+		return zapcore.NewJSONEncoder(config)
+	} else {
+		return zapcore.NewConsoleEncoder(config)
+	}
+}
+
+// ReplaceCores 替换为多种输出的Core
+func ReplaceCores(cores []zapcore.Core) zap.Option {
+	return zap.WrapCore(func(c zapcore.Core) zapcore.Core {
+		return zapcore.NewTee(cores...)
+	})
+}
+
+// GetLogPath 使用绝对路径
+func GetLogPath(dir string, files []string) []string {
+	if dir = strings.TrimSpace(dir); dir == "/dev/null" {
+		return nil
+	}
+	for i, file := range files {
+		files[i] = GetAbsPath(dir, file, true)
+	}
+	return files
+}
+
+func GetAbsPath(dir, file string, onlyFile bool) string {
+	if dir == "" && strings.HasPrefix(file, "std") {
+		return file
+	}
+	if strings.Contains(file, "%s") {
+		file = fmt.Sprintf(dir, file)
+	} else {
+		file = fmt.Sprintf("%s/%s", dir, strings.TrimPrefix(file, "/"))
+	}
+	u, err := url.Parse(file)
+	isFile := u.Scheme == "" || u.Scheme == "file"
+	if err == nil && isFile == false && onlyFile {
+		return file // 只能处理文件类型
+	}
+	path, _ := filepath.Abs(file)
+	path = ignoreWinDisk(path)
+	if isFile == false { // 重新拼接
+		path = fmt.Sprintf("%s://%s?%s", u.Scheme, path, u.RawQuery)
+	}
+	return path
+}
